18700af55ae144deb611d1755713e036e373886e
soporte <soporte@intranet.zubiaingenieros.com>
2022-01-14 13:55:32 +0100

    Filtrado 63

--- utils/curseapilinks.py
+++ utils/curseapilinks.py
@@ -1,36 +1,65 @@
+'''
+class CurseAPI:
+
+    key = '$2a$10$ku3.ncligCgUckN7vKnKyOeqH9y9H/aDca3t.QjPEG./wPaOU7UPu'
+    header = {'x-api-key': key, 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36'}
+
+    gameId = '432'      # Minecraft
+    classId = '6'       # Mods
+
+    pageSize = '20'
+    sortfield = '3'
+    sortOrder = 'desc'
+
+    endpoint = 'https://api.curseforge.com/v1'
+
+    minecraft_versions = 'https://addons-ecs.forgesvc.net/api/v2/minecraft/version'       # endpoint + '/minecraft/version'
+    minecraft_modid = endpoint + '/mods/'
+
+    search_base_query = endpoint + '/mods/search' + '?gameId=' + gameId + '&classId=' + classId + '&sortField=' + sortfield + '&sortOrder=' + sortOrder + '&pageSize=' + pageSize
+
+    search_filter_version = '&gameVersion='
+    search_offset = index = '&index='
+'''
+
 
 class CurseAPI:
     header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36'}
+
+    gameid = '432'
+    categorymods = '6'
+
+    updateorder = '2'
+    pagesize = '50'
+
     endpoint = 'https://addons-ecs.forgesvc.net/api/v2'
 
-    gameid = 432
-    categorymods = 6
-    updateorder = 2
-
     minecraft_versions = endpoint + '/minecraft/version'
-
     minecraft_modid = endpoint + '/addon/'
 
-    pagesize = 50
-    search_base_query = \
-        endpoint + \
-        '/addon/search' + \
-        '?gameId=' + str(gameid) +\
-        '&sectionId=' + str(categorymods) +\
-        '&sort=' + str(updateorder) +\
-        '&pageSize=' + str(pagesize)
+    search_base_query = endpoint + '/addon/search' + '?gameId=' + gameid + '&sectionId=' + categorymods + '&sort=' + updateorder + '&pageSize=' + pagesize
     search_filter_version = '&gameVersion='
     search_offset = index = '&index='
 
+'''
+gameId	            query	    integer(int32)	    false	    Filter by game id
+classId	            query	    integer(int32)	    false	    Filter by section id (discoverable via Game object)
+categoryId	        query	    integer(int32)	    false	    Filter by category id
+gameVersion	        query	    string	            false	    Filter by game version string
+searchFilter	    query	    string	            false	    Filter by free text search in the mod name and author
+sortField	        query	    any	                false	    Filter by ModsSearchSortField enumeration
+sortOrder	        query	    any	                false	    'asc' if sort is in ascending order, 'desc' if sort is in descending order
+modLoaderType	    query	    any	                false	    Optional - Filter only addons associated to a given modloader (Forge, Fabric ...)
+gameVersionTypeId	query	    integer(int32)	    false	    Filter only mods that contain files tagged with versions of the given gameVersionTypeId
+index	            query	    integer(int32)	    false	    A zero based index of the first item to include in the response
+pageSize	        query	    integer(int32)	    false	    The number of items to include in the response
 
-    '''
-    https://addons-ecs.forgesvc.net/api/v2/addon/search
-    ?gameId=432
-    &sectionId=6
-    &categoryId=0
-    &sort=2
-    &gameVersion=1.17
-    &pageSize=50
-    &index=0
-    &searchFilter={searchFilter}
-    '''
+1=Featured
+2=Popularity
+3=LastUpdated
+4=Name
+5=Author
+6=TotalDownloads
+7=Category
+8=GameVersion
+'''
